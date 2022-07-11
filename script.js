4ddf882f474f3ce73ef91ea6e2f781ac377597db
Miguel <miglegaspi12@gmail.com>
2022-07-11 15:40:39 -0400

    Basic html, css, and js

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+
+
+
+for (let i = 0; i < 256;i++) {
+    const square = document.createElement("div");
+    square.className = "square";
+    container.appendChild(square);
+}
