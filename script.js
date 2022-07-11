052169852c021aaf349000128250488e7e55abbb
Miguel <miglegaspi12@gmail.com>
2022-07-11 17:22:24 -0400

    Can draw

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const container = document.querySelector(".container");
 
 
 
+
 for (let i = 0; i < 256;i++) {
     const square = document.createElement("div");
-    square.className = "square";
+    square.className = "square";  
+    square.addEventListener("mouseover", down);
     container.appendChild(square);
+    
 }
+
+function down() {
+    this.className += (" colored");
+}
+
+
