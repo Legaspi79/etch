b1c347bc6bc47ec0d98d77b45d9fcd73684bf28c
Miguel <miglegaspi12@gmail.com>
2022-07-17 23:13:29 -0400

    Can Change Grid Size Now

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 const container = document.querySelector(".container");
+const gridBtn = document.querySelector(".btn");
+var count = 0;
+
+const slider = document.querySelector("#range");
+const sliderValue = document.querySelector("#sliderValue");
 
 
 
+slider.addEventListener("change", function () {
+    const columns = slider.value;
+    const gridSize = slider.value * slider.value;
+    container.style.gridTemplateColumns = "repeat(" + columns + ", 1fr)"
+    sliderValue.innerText = columns + " x " + columns;
+    container.innerHTML = "";
+    for (let i = 0; i < gridSize;i++) {
+        const square = document.createElement("div");
+        square.className = "square";  
+        square.addEventListener("mouseover", draw);
+        container.appendChild(square);
+    };
+    console.log(columns);
+})
 
-for (let i = 0; i < 256;i++) {
-    const square = document.createElement("div");
-    square.className = "square";  
-    square.addEventListener("mouseover", down);
-    container.appendChild(square);
-    
+function defaultGrid() {
+    for (let i = 0; i < slider.value * slider.value;i++) {
+        const square = document.createElement("div");
+        square.className = "square";  
+        square.addEventListener("mouseover", draw);
+        container.appendChild(square);
+    };
 }
+
+
+
+
 
 function down() {
-    this.className += (" colored");
+    
+};
+
+function draw() {
+    this.className += " colored";
 }
 
-
+defaultGrid();
