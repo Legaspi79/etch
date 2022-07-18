59be2dfaf8b923a1ea92fd7ee1cacc58c8dc5b2e
Miguel <miglegaspi12@gmail.com>
2022-07-18 14:19:25 -0400

    Colors working!

--- script.js
+++ script.js
@@ -2,19 +2,26 @@ const container = document.querySelector(".container");
 const gridBtn = document.querySelector(".btn");
 var count = 0;
 
-const slider = document.querySelector("#range");
-const sliderValue = document.querySelector("#sliderValue");
+const slider = document.querySelector("#gridRange");
+const gridSliderValue = document.querySelector("#gridSliderValue");
+;
+const colorSliderValue = document.querySelector("#colorRange");
 
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", clearAll);
+
+
+// colorSliderValue.addEventListener("input", changeColor)
 
 
 slider.addEventListener("change", function () {
     const columns = slider.value;
     const gridSize = slider.value * slider.value;
     container.style.gridTemplateColumns = "repeat(" + columns + ", 1fr)"
-    sliderValue.innerText = columns + " x " + columns;
+    gridSliderValue.innerText = columns + " x " + columns;
     container.innerHTML = "";
     for (let i = 0; i < gridSize;i++) {
-        const square = document.createElement("div");
+        var square = document.createElement("div");
         square.className = "square";  
         square.addEventListener("mouseover", draw);
         container.appendChild(square);
@@ -22,25 +29,31 @@ slider.addEventListener("change", function () {
     console.log(columns);
 })
 
+//change color
+// function changeColor() {
+//     console.log(colorSliderValue.value)
+//     let colorSquare = document.querySelectorAll("#colored")
+//     colorSquare.style.backgroundColor = "red";
+// }
+
 function defaultGrid() {
+    gridSliderValue.innerText = slider.value + " x " + slider.value;
     for (let i = 0; i < slider.value * slider.value;i++) {
         const square = document.createElement("div");
-        square.className = "square";  
+        square.className = "square"; 
         square.addEventListener("mouseover", draw);
         container.appendChild(square);
     };
 }
 
-
-
-
-
-function down() {
-    
-};
-
 function draw() {
-    this.className += " colored";
+    this.className = " colored";
+    this.style.backgroundColor = colorSliderValue.value;
+}
+
+function clearAll() {
+    container.innerHTML = "";
+    defaultGrid();
 }
 
 defaultGrid();
